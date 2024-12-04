e9eb4e0d332930e5819a45528c1a4bd65f607773
shanmu <shanmuha@gmail.com>
2024-12-04 08:57:04 +0000

    day4

--- src/day03.zig
+++ src/day03.zig
@@ -9,11 +9,46 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day03.txt");
+const small = @embedFile("data/day03small.txt");
+const small2 = @embedFile("data/day03small2.txt");
 
 pub fn main() !void {
-    
+    //part 1
+    const process = "do()" ++ data;
+    const sum = getMultSum(process);
+    print("part 1: {d}\n", .{sum});
+
+    var dosIter = tokenizeSeq(u8, process, "do()");
+    var part2Sum: i32 = 0;
+    while (dosIter.next()) |dosAndDonts| {
+        var dontIter = tokenizeSeq(u8, dosAndDonts, "don't()");
+        const dos = dontIter.next() orelse "";
+        part2Sum += getMultSum(dos);
+    }
+    print("part 2: {d}\n", .{part2Sum});
 }
 
+fn getMultSum(processData: []const u8) i32 {
+    var starts = tokenizeSeq(u8, processData, "mul(");
+    var sum: i32 = 0;
+    firstloop: while (starts.next()) |mul| {
+        var ends = tokenizeSca(u8, mul, ')');
+        const nums = ends.next();
+        if (nums == null) {
+            continue :firstloop;
+        } else {
+            var numsiter = tokenizeSca(u8, nums.?, ',');
+            const op1s = numsiter.next() orelse "0";
+            const op2s = numsiter.next() orelse "0";
+            const op1: i32 = parseInt(i32, op1s, 10) catch continue :firstloop;
+            const op2: i32 = parseInt(i32, op2s, 10) catch continue :firstloop;
+            if (op1 >= 0 and op1 < 1000 and op2 >= 0 and op2 < 1000) {
+                sum += op1 * op2;
+            }
+        }
+    }
+    return sum;
+}
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
 const tokenizeSeq = std.mem.tokenizeSequence;
--- src/day04.zig
+++ src/day04.zig
@@ -9,11 +9,138 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day04.txt");
+const small = @embedFile("data/day04small.txt");
 
 pub fn main() !void {
-    
-}
+    const process = data;
 
+    var wordsearch = List([]const u8).init(gpa);
+    defer _ = wordsearch.deinit();
+
+    var lines = tokenizeSca(u8, process, '\n');
+    while (lines.next()) |line| {
+        try wordsearch.append(line);
+    }
+    //var part1sum = 0;
+    var downs: u32 = 0;
+    var ups: u32 = 0;
+    var rights: u32 = 0;
+    var lefts: u32 = 0;
+    var rightups: u32 = 0;
+    var rightdowns: u32 = 0;
+    var leftups: u32 = 0;
+    var leftdowns: u32 = 0;
+    var masmas: u32 = 0;
+    var massam: u32 = 0;
+    var samsam: u32 = 0;
+    var sammas: u32 = 0;
+    for (0..wordsearch.items.len) |i| {
+        for (0..wordsearch.items[i].len) |j| {
+            if (is_xmas_right(wordsearch, i, j)) rights += 1;
+            if (is_xmas_left(wordsearch, i, j)) lefts += 1;
+            if (is_xmas_up(wordsearch, i, j)) ups += 1;
+            if (is_xmas_down(wordsearch, i, j)) downs += 1;
+            if (is_xmas_rightup(wordsearch, i, j)) rightups += 1;
+            if (is_xmas_rightdown(wordsearch, i, j)) rightdowns += 1;
+            if (is_xmas_leftup(wordsearch, i, j)) leftups += 1;
+            if (is_xmas_leftdown(wordsearch, i, j)) leftdowns += 1;
+        }
+    }
+    for (0..wordsearch.items.len - 2) |i| {
+        for (0..wordsearch.items[i].len - 2) |j| {
+            if (is_masmas(wordsearch, i, j)) masmas += 1;
+            if (is_massam(wordsearch, i, j)) massam += 1;
+            if (is_samsam(wordsearch, i, j)) samsam += 1;
+            if (is_sammas(wordsearch, i, j)) sammas += 1;
+        }
+    }
+    print("part 1:{d}\n", .{rights + lefts + ups + downs + rightups + rightdowns + leftups + leftdowns});
+    print("part 2:{d}\n", .{masmas + massam + samsam + sammas});
+}
+fn is_xmas_right(ws: List([]const u8), i: usize, j: usize) bool {
+    return (j < ws.items[i].len - 3) and
+        (ws.items[i][j] == 'X' and
+        ws.items[i][j + 1] == 'M' and
+        ws.items[i][j + 2] == 'A' and
+        ws.items[i][j + 3] == 'S');
+}
+fn is_xmas_left(ws: List([]const u8), i: usize, j: usize) bool {
+    return (j > 2) and
+        (ws.items[i][j] == 'X' and
+        ws.items[i][j - 1] == 'M' and
+        ws.items[i][j - 2] == 'A' and
+        ws.items[i][j - 3] == 'S');
+}
+fn is_xmas_down(ws: List([]const u8), i: usize, j: usize) bool {
+    return (i < ws.items.len - 3) and
+        (ws.items[i][j] == 'X' and
+        ws.items[i + 1][j] == 'M' and
+        ws.items[i + 2][j] == 'A' and
+        ws.items[i + 3][j] == 'S');
+}
+fn is_xmas_up(ws: List([]const u8), i: usize, j: usize) bool {
+    return (i > 2) and
+        (ws.items[i][j] == 'X' and
+        ws.items[i - 1][j] == 'M' and
+        ws.items[i - 2][j] == 'A' and
+        ws.items[i - 3][j] == 'S');
+}
+fn is_xmas_rightup(ws: List([]const u8), i: usize, j: usize) bool {
+    return (i > 2 and j < ws.items[i].len - 3) and
+        (ws.items[i][j] == 'X' and
+        ws.items[i - 1][j + 1] == 'M' and
+        ws.items[i - 2][j + 2] == 'A' and
+        ws.items[i - 3][j + 3] == 'S');
+}
+fn is_xmas_leftup(ws: List([]const u8), i: usize, j: usize) bool {
+    return (i > 2 and j > 2) and
+        (ws.items[i][j] == 'X' and
+        ws.items[i - 1][j - 1] == 'M' and
+        ws.items[i - 2][j - 2] == 'A' and
+        ws.items[i - 3][j - 3] == 'S');
+}
+fn is_xmas_rightdown(ws: List([]const u8), i: usize, j: usize) bool {
+    return (i < ws.items.len - 3 and j < ws.items[i].len - 3) and
+        (ws.items[i][j] == 'X' and
+        ws.items[i + 1][j + 1] == 'M' and
+        ws.items[i + 2][j + 2] == 'A' and
+        ws.items[i + 3][j + 3] == 'S');
+}
+fn is_xmas_leftdown(ws: List([]const u8), i: usize, j: usize) bool {
+    return (i < ws.items.len - 3 and j > 2) and
+        (ws.items[i][j] == 'X' and
+        ws.items[i + 1][j - 1] == 'M' and
+        ws.items[i + 2][j - 2] == 'A' and
+        ws.items[i + 3][j - 3] == 'S');
+}
+fn is_masmas(ws: List([]const u8), i: usize, j: usize) bool {
+    return (ws.items[i][j] == 'M' and
+        ws.items[i + 1][j + 1] == 'A' and
+        ws.items[i + 2][j + 2] == 'S' and
+        ws.items[i + 2][j] == 'M' and
+        ws.items[i][j + 2] == 'S');
+}
+fn is_massam(ws: List([]const u8), i: usize, j: usize) bool {
+    return (ws.items[i][j] == 'M' and
+        ws.items[i + 1][j + 1] == 'A' and
+        ws.items[i + 2][j + 2] == 'S' and
+        ws.items[i + 2][j] == 'S' and
+        ws.items[i][j + 2] == 'M');
+}
+fn is_samsam(ws: List([]const u8), i: usize, j: usize) bool {
+    return (ws.items[i][j] == 'S' and
+        ws.items[i + 1][j + 1] == 'A' and
+        ws.items[i + 2][j + 2] == 'M' and
+        ws.items[i + 2][j] == 'S' and
+        ws.items[i][j + 2] == 'M');
+}
+fn is_sammas(ws: List([]const u8), i: usize, j: usize) bool {
+    return (ws.items[i][j] == 'S' and
+        ws.items[i + 1][j + 1] == 'A' and
+        ws.items[i + 2][j + 2] == 'M' and
+        ws.items[i + 2][j] == 'M' and
+        ws.items[i][j + 2] == 'S');
+}
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
 const tokenizeSeq = std.mem.tokenizeSequence;
